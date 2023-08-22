69eac91f26d31bff24eefab09d7c6f1c820ef507
yuan.xin <yuanxin9997@qq.com>
2023-08-22 17:19:30 +0800

    add head

---
+++ src/09test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+#include "swap.h"
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int a = 10;
+    int b = 20;
+    swap(a, b);
+    return 0;
+}
---
+++ src/swap.h
@@ -0,0 +1,4 @@
+ #include <iostream>
+ using namespace std;
+ // 函数的声明
+ void swap(int a, int b);
