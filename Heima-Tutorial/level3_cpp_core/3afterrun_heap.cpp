cd99c124d1ab1a1a3ea39354312117ddb91a0add
yuan.xin <yuanxin9997@qq.com>
2023-09-06 22:49:11 +0800

    2023-09-06 new 操作符堆区申请内存，delete释放

--- Heima-Tutorial/level3_cpp_core/3afterrun_heap.cpp
+++ Heima-Tutorial/level3_cpp_core/3afterrun_heap.cpp
@@ -5,14 +5,19 @@
 using namespace std;
 int* func()
 {
-	int * a = new int(10); // new返回的是该数据类型的指针，在堆区创建整型数据
+//    int * a = new int;
+//    *a = 10;
+	int * a = new int(10); // new返回的是该数据类型的指针，在堆区创建整型数据，int括号内的是值
 	return a;
 }
 
 // 利用new操作符在堆区开辟数据
 void test01(){
-    int *p = func();
+//    string s = "0x600000c2c040";
+//    string *p1 = (*) &s;
 
+    int *p = func();
+    cout << "p=" << p << endl;
 	cout << *p << endl;
 	cout << *p << endl;
 	cout << *p << endl;
@@ -20,13 +25,15 @@ void test01(){
 
 	//利用delete释放堆区数据
 	delete p;
-
+    cout << "删除p之后" << endl;
 	cout << *p << endl; //报错，释放的空间不可访问
 }
 
 // 利用new在堆区开辟数组
 void test02(){
-    int* arr = new int[10];
+    cout << endl;
+    cout << " 利用new在堆区开辟数组" << endl;
+    int* arr = new int[10]; //[]表示创建数组，[]里面的数组表示数组的长度
 
 	for (int i = 0; i < 10; i++)
 	{
@@ -40,7 +47,7 @@ void test02(){
 	//释放数组 delete 后加 []
     cout << "fresh arr" << endl;
 	delete[] arr;
-
+    cout << "after fresh arr" << endl;
     for (int i = 0; i < 10; i++)
 	{
 		cout << arr[i] << endl;
@@ -50,7 +57,7 @@ void test02(){
 
 int main() {
 
-//	test01();
+	test01();
 
     test02();
 
