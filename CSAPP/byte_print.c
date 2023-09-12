f7e89004a04dd17cbadd8e7e59c1f835cc6a2735
yuan.xin <yuanxin9997@qq.com>
2023-09-12 17:14:55 +0800

    2023-9-12 01

--- CSAPP/byte_print.c
+++ CSAPP/byte_print.c
@@ -4,6 +4,7 @@
 
 
 #include "stdio.h"
+#include "string.h"
 typedef unsigned char * byte_pointer;
 
 void show_bytes(byte_pointer start, int len){
@@ -19,13 +20,20 @@ void show_int(int x){
 }
 
 void show_char(char c[]){
-    show_bytes(&c, sizeof(c));
+    show_bytes((byte_pointer)&c, sizeof(c));
 }
 
 int main(){
     show_int(12345);
 //    char c = 'a'; // 字符型
-    char c[] = "abcde"; // 字符串型
+    char c[] = "abcde"; // 字符串型，注意变量的定义区别
     show_char(c);
+    show_bytes((byte_pointer)&c, strlen(c));
+    show_bytes((byte_pointer)&c, sizeof(c));
+
+//    const char *s="abcde";
+    char *s="abcde";
+    show_bytes((byte_pointer)s, strlen(s));
+
     return 0;
 }
