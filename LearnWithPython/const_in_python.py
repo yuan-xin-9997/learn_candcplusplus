2c7cc4ec87bc8c0ac838164f2808cdf37e3dd7fd
yuan.xin <yuanxin9997@qq.com>
2023-09-13 13:08:54 +0800

    2023-9-13 删除Python RLock

--- LearnWithPython/const_in_python.py
+++ LearnWithPython/const_in_python.py
@@ -20,49 +20,5 @@ import time
 import threading
 
 constant.MAX_VALUE = 100
-# constant.MAX_VALUE = 50  # 修改变量将报错
-# constant.var = 10   # 常量名小写报错
-
-######################################################################
-# Python 多线程中的锁
-
-# 生成一个递归对象
-Rlock = threading.RLock()
-
-
-class MyThread(threading.Thread):
-    def __init__(self):
-        threading.Thread.__init__(self)
-
-    def run(self) -> None:
-        self.fun_A()
-        self.fun_B()
-
-    def fun_A(self):
-        Rlock.acquire()
-        print('A加锁1', end='\t')
-        Rlock.acquire()
-        print('A加锁2', end='\t')
-        time.sleep(0.2)
-        Rlock.release()
-        print('A释放1', end='\t')
-        Rlock.release()
-        print('A释放2')
-
-    def fun_B(self):
-        Rlock.acquire()
-        print('B加锁1', end='\t')
-        Rlock.acquire()
-        print('B加锁2', end='\t')
-        time.sleep(3)
-        Rlock.release()
-        print('B释放1', end='\t')
-        Rlock.release()
-        print('B释放2')
-
-
-if __name__ == '__main__':
-    t1 = MyThread()
-    t2 = MyThread()
-    t1.start()
-    t2.start()
+constant.MAX_VALUE = 50  # 修改变量将报错
+constant.var = 10   # 常量名小写报错
